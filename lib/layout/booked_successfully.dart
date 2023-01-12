34a9ecd1b5206dc568094a761ca4da492dd910fb
Mohamed Ramadan <MoRm9n@gmail.com>
2023-01-12 20:05:23 +0200

    add successfully screen

---
+++ lib/layout/booked_successfully.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:irl/layout/irl_app/irl_app.dart';
+
+class BookedSuccessfully extends StatelessWidget {
+  const BookedSuccessfully({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 13.0,
+        // this need to be set
+        backgroundColor: Colors.transparent,
+        // create gradient background color
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Colors.brown.shade600.withOpacity(0.5),
+                Colors.white.withOpacity(0.4),
+                Colors.brown.shade600.withOpacity(0.7)
+              ],
+            ),
+          ),
+        ),
+        title: const Center(
+          child: Text(
+            "Booked Successfully",
+            style: TextStyle(
+              fontFamily: "Lora",
+              color: Colors.brown,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/done_booking.png"),
+            const Text(
+              "You have Successfully Booked Your Appointment",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.brown)),
+                onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (_) => const IrlAppScreen()),
+                    (route) => false),
+                child: const Text("Move To Home"))
+          ],
+        ),
+      ),
+    );
+  }
+}
